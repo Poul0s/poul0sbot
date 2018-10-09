d3116fa6d0dd5e69dc3e58f3034fe42e5d4eb7f0
Poul0s <43917783+Poul0s@users.noreply.github.com>
2018-10-09 17:56:28 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -122,7 +122,7 @@ PS:La commande help n'est pas terminer.`)
 
 bot.on('message', function (message) {
     if (message.content === prefix + 'join') {
-        message.channel.send(`Si tu veux venir sur mon discord join sur https://discord.gg/yJBdh6z`)
+        message.PrivateDM.send(`Si tu veux venir sur mon discord join sur https://discord.gg/yJBdh6z`)
     }
 })
 
