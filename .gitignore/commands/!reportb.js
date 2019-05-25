5d0f7fc1e30c996473c61211b5d1bd3a358207fe
Poul0s <paulsalame33200@gmail.com>
2019-05-25 12:12:56 +0200

    Update !reportb.js

--- .gitignore/commands/!reportb.js
+++ .gitignore/commands/!reportb.js
@@ -3,10 +3,9 @@ module.exports.run = async (bot, message, args) => {
 if(message.mentions.users.size === 0) return message.reply("Tu n'as pas mentionné d'utilisateur'").catch(console.error);
         let userreport = message.mentions.users.first()
         let raison = message.content.split(" " + userreport + " ").slice(1)
-        let raison1 = raison[1];
         if(!raison) return message.reply("Tu n'as pas mis de raison")
         message.reply("Votre report a bien été envoyé.")
-            bot.users.get("519186194886688779").send(`${userreport.id} ${userreport.username} à été report sur le serveur ${message.guild.name} pour ^${raison1}`);
+            bot.users.get("519186194886688779").send(`${userreport.id} ${userreport.username} à été report sur le serveur ${message.guild.name} par ${message.author.id} pour ${raison}`);
 }
 module.exports.help = {
     name: "reportb"
